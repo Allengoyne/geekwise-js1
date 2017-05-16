99ec3f29fcbd475d4ba73bec98d185828c75b003
Allen Goyne <allen.goyne@gmail.com>
2017-05-15 20:57:38 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,46 @@
+// ES6 - ECMAScript 6
+
+//  Good
+//
+// var lowercase;
+// var camelCase;
+// var use_underscores;
+// var $dollarsign;
+//
+// // Meh
+// var SuperCamelCase;
+//
+// // Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log('Sending a message to the console.');
+console.info('Sending and information message');
+console.warn('sending a warning');
+console.error('Danger Will Robinson');
+
+var
+
+
+          freedom
+
+
+
+     =
+
+
+     "A string of" +
+"" +
+          "javascript";
+
+     console.log(freedom);
+
+     var javascriptTemplateString = `
+and
+type whatever you
+
+want
+
+     `;
+     console.log(javascriptTemplateString);
